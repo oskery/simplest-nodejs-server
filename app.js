6a30c119cff33e7b1a69545ae79812a8e300e1a4
oskery <oskar.lyrstrand@gmail.com>
2020-01-30 11:47:17 +0100

    Show more info to user

--- app.js
+++ app.js
@@ -3,17 +3,19 @@ const express = require("express");
 const app = express();
 app.use(express.static("assets"));
 
-const htmlPageWithAppLinks = `<div style="height: 100%; display: flex; flex-direction: column; align-items: center; justify-content: center;"><a href="http://apple.com"><img style="width: 50vw; max-width:568px" src="images/Download_on_the_App_Store_Badge_SE_RGB_blk_100317.svg" /></a><a href="http://google.com"><img style="width: 57vw; max-width:646px" src="images/google-play-badge.png" /></a></div>`;
+const htmlPageWithAppLinks = (uOS, uParams) => `<div style="height: 100%; display: flex; flex-direction: column; align-items: center; justify-content: center;"><p>${uOS},<br /> ${uParams}</p><a href="http://apple.com"><img style="width: 50vw; max-width:568px" src="images/Download_on_the_App_Store_Badge_SE_RGB_blk_100317.svg" /></a><a href="http://google.com"><img style="width: 57vw; max-width:646px" src="images/google-play-badge.png" /></a></div>`;
 
 app.get("/", (req, res) => {
   let os = deviceOS(req);
-  if (req.query.facility && req.query.door && (os === "iphone" || os === "ipad" || os === "ipod")) {
+  if (req.query.facility && req.query.door) {
+    if (os === "iphone" || os === "ipad" || os === "ipod") {
     res.redirect(
       `exp://192.168.137.173:19000?facility=${req.params.facility}&door=1`
     );
+    }
   }
   else {
-    res.send(htmlPageWithAppLinks);
+    res.send(htmlPageWithAppLinks(os, `facility: ${req.query.facility}, door: ${req.query.door}`));
   }
 });
 
